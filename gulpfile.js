ab26cae208418c74c2557eaf514e9edc89b1e231
bembolat <barkadwood@gmail.com>
2018-09-28 12:16:05 +0300

    Добавляет изменения

--- gulpfile.js
+++ gulpfile.js
@@ -5,102 +5,29 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var minify = require("gulp-csso");
-var rename = require("gulp-rename");
-var imagemin = require("gulp-imagemin");
-var webp = require("gulp-webp");
-var svgstore = require("gulp-svgstore");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
-var run = require("run-sequence");
-var del = require("del");
-
 var server = require("browser-sync").create();
 
-gulp.task("build", function(done) {
-  run("delete",
-      "copy",
-      "style",
-      "images",
-      "sprite",
-      "html",
-      "webp",
-      done);
-});
-
-gulp.task("delete", function() {
-  return del("build");
-});
-
-gulp.task("copy", function() {
-  return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**"
-  ], {
-    base: "source"
-  })
-    .pipe (gulp.dest("build"));
-});
 
 gulp.task("style", function() {
-  gulp.src("source/less/style.less")
+  gulp.src("less/style.less")
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(rename(function (path) {
-      path.basename += ".min";
-      path.extname = ".css"
-    }))
-    .pipe(minify())
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("css"))
     .pipe(server.stream());
 });
 
-gulp.task("sprite", function() {
-  return gulp.src("source/img/ico-*.svg")
-    .pipe(svgstore({
-    inlineSvg: true
-  }))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("html", function() {
-  return gulp.src("source/*.html")
-    .pipe(posthtml([
-    include()
-  ]))
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("images", function() {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-    imagemin.optipng({optimizationLevel: 3}),
-    imagemin.jpegtran({progressive: true})
-  ]))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("webp", function() {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("build/img"));
-});
-
-gulp.task("serve", function() {
+gulp.task("serve", ["style"],function() {
   server.init({
-    server: "build/",
+    server: ".",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/less/**/*.less", ["style"]);
-  gulp.watch("source/*.html", ["html"]);
-});
+  gulp.watch("less/**/*.less", ["style"]);
+  gulp.watch("*.html").on("change", server.reload);
+})
