d7a0dfef17839807e740a0d320243b2dff776c41
bembolat <barkadwood@gmail.com>
2018-11-03 10:44:57 +0300

    Автоматизация и оптимизация

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,38 @@ var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
+var posthtml = require("gulp-posthtml");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var webp =require("gulp-webp"); 
+var run = require("run-sequence");
+var del = require("del");
 
+gulp.task("build", function(done) {
+  run("clean", "copy", "style", "html", done);
+});
+
+gulp.task("html", function() {
+  return gulp.src("*.html")
+  .pipe(gulp.dest("build/"));
+})
+
+gulp.task("copy", function() {
+  return gulp.src([
+    "fonts/**/*.{woff,woff2}",
+    "img/**",
+    "js/**"
+    ], {
+      base: "."
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -15,13 +44,22 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(gulp.dest("build/css"))
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
 });
 
-gulp.task("serve", ["style"],function() {
+gulp.task("webp", function() {
+  return gulp.src("img/**/*.{png,jpg}")
+  .pipe(webp({quality: 85}))
+  .pipe(gulp.dest("img"));
+});
+
+
+gulp.task("serve", function() {
   server.init({
-    server: ".",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -29,5 +67,5 @@ gulp.task("serve", ["style"],function() {
   });
 
   gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
-})
+  gulp.watch("*.html", ["html"]);
+});
